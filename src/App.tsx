b82fb8118ca727ec52ba67b5d2c99683d94e2008
preston176 <prestonnyamweya@gmail.com>
2024-04-03 22:41:22 +0300

    Add routing and new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Home from './Components/Home/Home';
+import DemoHeader from './Components/Demo/DemoHeader';
+import Header from './Components/Home/Header';
+import Demo from './Components/Demo/Demo';
 
 const App = () => {
+  const auth = false;
   return (
-    <div>
-      <h1>Welcome to my blog App</h1>
-    </div>
+    <>
+      {auth ? <Header /> : <DemoHeader />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='demo' element={<Demo />} />
+      </Routes>
+    </>
   )
 }
 
