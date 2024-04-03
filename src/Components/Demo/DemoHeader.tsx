092377727aa07634b0fbf59d2d6342d763db28ad
preston176 <prestonnyamweya@gmail.com>
2024-04-03 23:14:00 +0300

    Update DemoHeader component styling and add new colors and fonts

--- src/Components/Demo/DemoHeader.tsx
+++ src/Components/Demo/DemoHeader.tsx
@@ -4,21 +4,25 @@ import { nav } from './../../demoData.ts';
 
 const DemoHeader = () => {
     return (
-        <div className="border-b border-black sticky top-0 z-50">
+        <div className="border-b border-black sticky top-0 z-50 bg-banner">
             <div className="w-[95%] md:w-[90%] mx-auto h-[70px] flex items-center justify-between">
                 <Link to={'/'}>
                     <img
                         className="h-[2.5rem]"
                         src="https://i.pinimg.com/736x/af/09/41/af0941ba06185e9846af909ba9350baf.jpg" alt="site_logo" />
                 </Link>
-                <div className="">
-                    <div className="">
+                <div className="flex items-center gap-5">
+                    <div className="hidden text-sm sm:flex items-center gap-5">
                         {
                             nav.map((item: { path: string, title: string }, index: number) => (
-                                <Link to={item.path} key={index} className="mx-2">{item.title}</Link>
+                                <Link to={item.path} key={index}>{item.title}</Link>
                             ))
 
                         }
+                    </div>
+                    <div className="relative flex gap-x-5">
+                        <button className="hidden text-sm sm:flex items-center gap-5">Log In</button>
+                        <button className="bg-black text-white rounded-full px-3 p-2 text-sm font-medium">Get Started</button>
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black1: "rgba(0, 0, 0, 0.87)",
+        banner: "rgb(19,206,224)"
+      },
+      fontFamily: {
+        title: "Poppins, Times New Roman, sans-serif",
+        texts: "Roboto, sans-serif",
+      },
+      gridTemplateColumns: {
+        card: "repeat(auto-fit, minmax(280px, 1fr))",
+      },
+    },
   },
   plugins: [],
 }
