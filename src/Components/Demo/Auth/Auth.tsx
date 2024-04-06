ef2c6fd1c2ab3866dd378f304e08cce2ec58a725
preston176 <prestonnyamweya@gmail.com>
2024-04-06 16:02:56 +0300

    Add sign-in and sign-up demo functionality in Auth component

--- src/Components/Demo/Auth/Auth.tsx
+++ src/Components/Demo/Auth/Auth.tsx
@@ -5,9 +5,12 @@ import { LiaTimesSolid } from "react-icons/lia"
 import { AiOutlineMail } from "react-icons/ai";
 import { MdFacebook } from "react-icons/md";
 import { useState } from "react";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 
 const Auth = () => {
     const [createUser, setCreateUser] = useState<boolean>(false);
+    const [signReq, setSignReq] = useState<string>('');
     return (
         <Modal>
             <section className="z-50 fixed top-0 bottom-0 left-0 md:left-[10rem] overflow-auto right-0 md:right-[10rem] bg-white shadow">
@@ -15,15 +18,31 @@ const Auth = () => {
                     <LiaTimesSolid />
                 </button>
                 <div className="flex flex-col justify-center items-center gap-[3rem]">
-                    <h2 className="text-2xl pt-[5rem]">{createUser ? "Join Today" : "Welcome Back"}</h2>
-                    <div className="flex flex-col gap-2 w-fit mx-auto">
-                        <Button icon={<FcGoogle className="text-xl" />} text={`${createUser ? "Sign Up" : "Continue"} With Google`} />
-                        <Button icon={<MdFacebook className="text-xl" />} text={`${createUser ? "Sign Up" : "Continue"} With FaceBook`} />
-                        <Button icon={<AiOutlineMail className="text-xl" />} text={`${createUser ? "Sign Up" : "Log In"} With E-Mail`} />
-                    </div>
-                    <p>
-                        {createUser ? "Don't have an account ?" : "Already have an account?"} <button onClick={() => setCreateUser(prevstate => !prevstate)} className="text-blue-500 cursor-pointer font-bold">{createUser ? "Sign Up" : "Log In"}</button>
-                    </p>
+                    {
+                        signReq === '' ? (
+                            <>
+                                <h2 className="text-2xl pt-[5rem]">{createUser ? "Join Today" : "Welcome Back"}</h2>
+                                <div className="flex flex-col gap-2 w-fit mx-auto">
+                                    <Button
+                                        click={() => setSignReq(createUser ? "sign-up" : "sign-in")}
+                                        icon={<FcGoogle className="text-xl" />} text={`${createUser ? "Sign Up" : "Continue"} With Google`} />
+                                    <Button
+                                        click={() => setSignReq(createUser ? "sign-up" : "sign-in")}
+                                        icon={<MdFacebook className="text-xl" />} text={`${createUser ? "Sign Up" : "Continue"} With FaceBook`} />
+                                    <Button
+                                        click={() => setSignReq(createUser ? "sign-up" : "sign-in")}
+                                        icon={<AiOutlineMail className="text-xl" />} text={`${createUser ? "Sign Up" : "Continue"} With E-Mail`} />
+                                </div>
+                                <p>
+                                    {createUser ? "Don't have an account ?" : "Already have an account?"} <button onClick={() => setCreateUser(prevstate => !prevstate)} className="text-blue-500 cursor-pointer font-bold">{createUser ? "Sign Up" : "Log In"}</button>
+                                </p>
+                            </>
+                        ) : signReq === "sign-in" ?
+                            (
+                                <SignIn />
+                            )
+                            : signReq === "sign-up" ?
+                                (<SignUp />) : null}
                 </div>
             </section>
         </Modal>
--- src/Components/utils/AuthButton/Button.tsx
+++ src/Components/utils/AuthButton/Button.tsx
@@ -1,12 +1,15 @@
 interface props {
     icon: JSX.Element;
     text: string;
+    click: () => void;
 }
 
 
-const Button = ({ icon, text }: props) => {
+const Button = ({ icon, text, click }: props) => {
     return (
-        <div className='flex items-center gap-10 sm:w-[20rem] border border-black px-3 py-2 rounded-full cursor-pointer hover:scale-105 transition-all duration-150 hover:text-white hover:bg-black hover:border-none'>
+        <div
+            onClick={click}
+            className='flex items-center gap-10 sm:w-[20rem] border border-black px-3 py-2 rounded-full cursor-pointer hover:scale-105 transition-all duration-150 hover:text-white hover:bg-black hover:border-none'>
             {icon} {text}
         </div>
     )
