7a1b1b1f4fcc91589700024e591e2cdc7c9ea7b9
preston176 <prestonnyamweya@gmail.com>
2024-04-30 20:12:01 +0300

    added click away to close any modal dialogs

--- src/Components/utils/Modal.tsx
+++ src/Components/utils/Modal.tsx
@@ -6,9 +6,9 @@ interface ModalProps {
     setModal: (value: boolean) => void
 }
 
-const Modal: React.FC<ModalProps> = ({ children, modal }) => {
+const Modal: React.FC<ModalProps> = ({ children, modal, setModal }) => {
     return (
-        <div className={`bg-white/50 fixed inset-0 z-10 transition-all duration-700 ${modal ? "visible opacity-100" : "invisible opacity-0"}`}>
+        <div onClick={() => setModal(false)} className={`bg-white/50 fixed inset-0 z-10 transition-all duration-700 ${modal ? "visible opacity-100" : "invisible opacity-0"}`}>
             {children}
         </div>
     );
