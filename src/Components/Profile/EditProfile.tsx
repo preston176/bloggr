bd771b35e6141995e542f9a355560b32509774d2
preston176 <prestonnyamweya@gmail.com>
2024-04-30 22:14:48 +0300

    handle form submission and display user data dynamically

--- src/Components/Profile/EditProfile.tsx
+++ src/Components/Profile/EditProfile.tsx
@@ -3,15 +3,16 @@ import Modal from './../utils/Modal';
 import { useEffect, useRef, useState } from 'react';
 import { UserDetails } from '../../Context/Context';
 import { toast } from 'react-toastify';
-import ToastNotifier from '../Common/ToastNotifier';
 import { db, storage } from './../../firebase/firebase';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { doc, updateDoc } from 'firebase/firestore';
+import Loading from '../Loading/Loading';
 
-const EditProfile = ({ editModal, setEditModal, getUserData }: {
+const EditProfile = ({ editModal, setEditModal, getUserData, setModal }: {
     editModal: boolean;
     setEditModal: (value: boolean) => void;
     getUserData: UserDetails | undefined;
+    setModal: React.Dispatch<React.SetStateAction<boolean>>
 }) => {
     const imgRef = useRef<HTMLInputElement>(null)
     const openFile = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -24,6 +25,7 @@ const EditProfile = ({ editModal, setEditModal, getUserData }: {
         userImg: null,
         bio: ""
     })
+    const [loading, setLoading] = useState<boolean>(false);
 
     // check if there is data in the database and set the form state
     useEffect(() => {
@@ -50,28 +52,29 @@ const EditProfile = ({ editModal, setEditModal, getUserData }: {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-    
+
         try {
             if (form.username === "" || form.bio === "") {
                 toast.error("Please fill all fields");
                 return;
             }
-    
+
+            setLoading(true);
             // Check if userImg is not null before uploading
             if (form.userImg) {
                 // Upload image to storage
                 const storageRef = ref(storage, `image/${form.userImg.name}`);
                 await uploadBytes(storageRef, form.userImg);
-    
+
                 // Get image download URL
                 const imageUrl = await getDownloadURL(storageRef);
-    
+
                 // Update user data in the database with image URL
                 const docRef = doc(db, "users", getUserData?.userId || "");
                 await updateDoc(docRef, {
                     username: form.username,
                     bio: form.bio,
-                    userImg: imageUrl,
+                    userImg: imgUrl ? imageUrl : form.userImg,
                 });
             } else {
                 // Update user data in the database without image URL
@@ -81,32 +84,41 @@ const EditProfile = ({ editModal, setEditModal, getUserData }: {
                     bio: form.bio,
                 });
             }
-    
-            toast.success("Profile updated successfully");
+
+            setLoading(false);
             setEditModal(false);
+            setModal(false)
+            toast.success("Profile updated successfully");
         } catch (error) {
             console.error("Error updating profile: ", error);
             toast.error("An error occurred while updating your profile");
+            setModal(false);
         }
     };
-    
+
 
 
     return (
         <Modal modal={editModal} setModal={setEditModal}>
-            <ToastNotifier />
-            <form onSubmit={handleSubmit} className="center w-[95%] md:w-[45rem] bg-white mx-auto shadows my-[1rem] z-20 mb-[3rem] p-[2rem]">
+
+            {loading ? <Loading /> : <form onSubmit={handleSubmit} className="center w-[95%] md:w-[45rem] bg-white mx-auto shadows my-[1rem] z-20 mb-[3rem] p-[2rem]">
                 {/* head */}
                 <div className="flex items-center justify-between">
                     <h2 className='font-bold text-xl'>Profile Info</h2>
-                    <button className='text-xl' onClick={() => setEditModal(false)}><LiaTimesSolid /></button>
+                    <button type='button' className='text-xl' onClick={() => setEditModal(false)}><LiaTimesSolid /></button>
                 </div>
                 {/* body */}
                 <section className='mt-6'>
                     <p className="pb-3 text-sm text-gray-500">Photo</p>
                     <div className="flex gap-[2rem]">
                         <div className="w-[5rem]">
-                            <img className='min-h-[5rem] min-w-[5rem] object-cover border border-gray-400 rounded-full' src={imgUrl ? imgUrl : "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"} alt="profile-image" />
+                            <img
+                                className='min-h-[5rem] min-w-[5rem] object-cover border border-gray-400 rounded-full'
+                                src={getUserData ? getUserData?.userImg : form.userImg ? URL.createObjectURL(form.userImg) : "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"}
+                                alt="profile-image"
+                            />
+
+
                             <input
                                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                                     e.preventDefault(); // Prevent default form submission behavior
@@ -176,7 +188,7 @@ const EditProfile = ({ editModal, setEditModal, getUserData }: {
                     }} className='border border-green-600 py-2 px-5 rounded-full text-green-600' type='button'>Cancel</button>
                     <button className='border border-green-600 py-2 px-5 rounded-full bg-green-800 text-white' type='submit' >Save Changes</button>
                 </div>
-            </form>
+            </form>}
 
         </Modal >
     )
