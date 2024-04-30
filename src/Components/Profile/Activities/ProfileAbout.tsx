5bd053dfc361fe8332124daf81b632e15d6427c5
preston176 <prestonnyamweya@gmail.com>
2024-04-30 20:30:33 +0300

    display user bio and add edit button

--- src/Components/Profile/Activities/ProfileAbout.tsx
+++ src/Components/Profile/Activities/ProfileAbout.tsx
@@ -1,7 +1,21 @@
+import { UserDetails } from "../../../Context/Context";
 
-const ProfileAbout = () => {
+const ProfileAbout = ({ getUserData, setEditModal }: {
+  getUserData: UserDetails | undefined;
+  setEditModal: React.Dispatch<React.SetStateAction<boolean>>
+}) => {
   return (
-    <div>ProfileAbout</div>
+    <div className="w-full">
+      <p className="text-2xl first-letter::uppercase">
+        {getUserData?.bio || getUserData?.username + "No bio available"}
+      </p>
+      <div className="text-right">
+
+        <button
+          onClick={() => setEditModal(true)}
+          className="border border-black py-2 px-5 rounded-full text-black mt-[3rem]">Edit</button>
+      </div>
+    </div>
   )
 }
 
