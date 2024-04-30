f107a90ce7c6ba7fc413e9b283e94b5a45c8f44e
preston176 <prestonnyamweya@gmail.com>
2024-04-30 16:57:55 +0300

    Implemented edit profile details modal dialog

--- src/Components/Profile/EditProfile.tsx
+++ src/Components/Profile/EditProfile.tsx
@@ -1,11 +1,68 @@
+import { LiaTimesSolid } from 'react-icons/lia';
 import Modal from './../utils/Modal';
+import { useRef, useState } from 'react';
 
 const EditProfile = ({ editModal, setEditModal }: {
     editModal: boolean,
     setEditModal: (value: boolean) => void
 }) => {
+    const imgRef = useRef<HTMLInputElement>(null)
+    const openFile = () => {
+        imgRef?.current?.click()
+    }
+    const [imgUrl, setImgUrl] = useState<string>("")
+
     return (
-        <Modal modal={editModal} setModal={setEditModal}>EditProfile</Modal>
+        <Modal modal={editModal} setModal={setEditModal}>
+
+            <div className="center w-[95%] md:w-[45rem] bg-white mx-auto shadows my-[1rem] z-20 mb-[3rem] p-[2rem]">
+                {/* head */}
+                <div className="flex items-center justify-between">
+                    <h2 className='font-bold text-xl'>Profile Info</h2>
+                    <button className='text-xl' onClick={() => setEditModal(false)}><LiaTimesSolid /></button>
+                </div>
+                {/* body */}
+                <section className='mt-6'>
+                    <p className="pb-3 text-sm text-gray-500">Photo</p>
+                    <div className="flex gap-[2rem]">
+                        <div className="w-[5rem]">
+                            <img className='min-h-[5rem] min-w-[5rem] object-cover border border-gray-400 rounded-full' src={imgUrl ? imgUrl : "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"} alt="profile-image" />
+                            <input
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                    if (e.target.files && e.target.files.length > 0) {
+                                        const file = e.target.files[0];
+                                        const imgUrl = URL.createObjectURL(file);
+                                        setImgUrl(imgUrl);
+                                    }
+                                }}
+                                accept='image/jpg, image/png, image/jpeg' ref={imgRef} type="file" hidden />
+                        </div>
+                        <div>
+                            <div className="flex gap-4 text-sm">
+                                <button onClick={openFile} className='text-green-600'>Update</button>
+                                <button className='text-red-600'>Remove</button>
+                            </div>
+                            <p className='w-full sm:w-[20rem] text-gray-500 text-sm pt-2'>Recommended: Square PNG, JPG or JPEG</p>
+                        </div>
+                    </div>
+                </section>
+                {/* prof edit form */}
+                <section className='pt-[1rem] text-sm'>
+                    <label className='pb-3 block' htmlFor="">Name</label>
+                    <input className='p-1 border-b border-black w-full outline-none' type="text" placeholder='Username ...' maxLength={50} />
+                    <p className='text-sm text-gray-600 pt-2'>This will appear on your Profile page, as well as your byline, and in your responses. 10/50</p>
+                    <label className='pb-3 block' htmlFor="">Bio</label>
+                    <input className='p-1 border-b border-black w-full outline-none' type="text" placeholder='Your Bio ...' maxLength={50} />
+                    <p className='text-sm text-gray-600 pt-2'>This will appear on your Profile page and next to your stories. 42/160</p>
+                </section>
+                {/* footer */}
+                <div className="flex items-center justify-end gap-4 pt-[2rem]">
+                    <button className='border border-green-600 py-2 px-5 rounded-full text-green-600'>Cancel</button>
+                    <button className='border border-green-600 py-2 px-5 rounded-full bg-green-800 text-white'>Save Changes</button>
+                </div>
+            </div>
+
+        </Modal>
     )
 }
 
