713c9551111ac38ddc1a530be3c86f63bd347932
preston176 <prestonnyamweya@gmail.com>
2024-05-10 16:38:27 +0300

    added event listener to the "add image" placeholder to store blog image

--- src/Components/Home/Write/Preview.tsx
+++ src/Components/Home/Write/Preview.tsx
@@ -1,6 +1,12 @@
 import { LiaTimesSolid } from "react-icons/lia"
+import { useRef } from "react";
 
 const Preview = () => {
+    const imageRef = useRef<HTMLInputElement>(null)
+    const handleClick = () => {
+        imageRef?.current?.click()
+    }
+
     return (
         <section
             className="absolute inset-0 bg-white z-30"
@@ -10,9 +16,10 @@ const Preview = () => {
                 <div className="mt-[8rem] flex flex-col md:flex-row gap-10">
                     <div className="flex-[1]">
                         <h3>Story Preview</h3>
-                        <div className="w-full h-[200px] object-cover bg-gray-100 my-3 grid place-items-center cursor-pointer bg-cover bg-no-repeat">
+                        <div onClick={() => handleClick} className="w-full h-[200px] object-cover bg-gray-100 my-3 grid place-items-center cursor-pointer bg-cover bg-no-repeat">
                             Add Image
                         </div>
+                        <input ref={imageRef} type="file" hidden />
                     </div>
                     <div className="flex-[1]"></div>
                 </div>
