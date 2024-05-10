bea74758e0ff172bcd2086524c4b61eaad4d452f
preston176 <prestonnyamweya@gmail.com>
2024-05-10 16:34:13 +0300

    write.tsx component defined and styled

--- src/Components/Home/Write/Write.tsx
+++ src/Components/Home/Write/Write.tsx
@@ -1,8 +1,26 @@
-
+import { useState } from "react"
+import ReactQuill from "react-quill"
+import Preview from "./Preview"
 const Write = () => {
-  return (
-    <div>Write</div>
-  )
+    const [description, setDescription] = useState<string>("")
+    return (
+        <section
+            className="w-[90%] md:w-[90%] lg:w-[60%] mx-auto py-[3rem]"
+        >
+            <input type="text" placeholder="Title" className="text-4xl outline-none w-full" />
+
+            <ReactQuill
+                theme="bubble"
+                value={description}
+                onChange={setDescription}
+                className="write my-5"
+                placeholder="Describe Your Story ..."
+            />
+            <div className="">
+                <Preview />
+            </div>
+        </section>
+    )
 }
 
 export default Write
