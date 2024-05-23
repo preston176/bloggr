08959245cf314824629a8213a71ff30ac325c7f5
preston176 <prestonnyamweya@gmail.com>
2024-05-23 17:07:44 +0300

    posts tsx files defined

--- src/Components/Common/Posts/Posts.tsx
+++ src/Components/Common/Posts/Posts.tsx
@@ -1,9 +1,20 @@
-const Posts = () => {
-    return (
-        <div>
-            Posts
-        </div>
-    )
-}
+import React from "react";
+import Loading from "../../Loading/Loading";
+import PostsCard from "./PostsCard";
+import { Blog } from "../../../Context/Context";
 
-export default Posts
+const Posts = () => {
+  const { postData, postLoading } = Blog();
+  return (
+    <section className="flex flex-col gap-[2.5rem]">
+      {postLoading ? (
+        <Loading />
+      ) : (
+        postData &&
+        postData?.map((post, i) => <PostsCard post={post} key={i} />)
+      )}
+    </section>
+  );
+};
+
+export default Posts;
