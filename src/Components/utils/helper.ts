b6ec300ca2494598d449195fca6b1389687330e9
preston176 <prestonnyamweya@gmail.com>
2024-05-23 17:11:45 +0300

    utils folder removed

--- src/Components/utils/helper.ts
+++
@@ -1,5 +0,0 @@
-export const secretEmail = (email: string) => {
-    const [username, domain] = email.split("@");
-    const secretUser = username.substring(0, 2) + "*".repeat(username.length - 2);
-    return `${secretUser}@${domain}`;
-  };
